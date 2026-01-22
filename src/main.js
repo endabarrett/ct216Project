2d0c2b73d1395807f70c37869effa3a146a73e86
Enda Barrett <barrettenda@gmail.com>
2026-01-22 11:54:59 +0000

    Week 14

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './router/routes'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 let router = createRouter({
     history: createWebHistory(),
--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -3,7 +3,32 @@
 </script>
 <template>
     <Nav />
-    <h1>This is my Blog page</h1>
-    <p>Welcome to my wonderful blog page</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                First
+            </div>
+            <div class="col-md-6">
+                Second
+            </div>
+        </div>    
+        <h1>This is my Blog page</h1>
     
+        <p id="welcome" class="myBlueClass">Welcome to my wonderful blog page</p>
+
+        <a href="https://www.google.ie">The best engine</a>
+        <button class="btn btn-primary">My button</button>
+    </div>
 </template>
+
+<style scoped>
+.myBlueClass {
+    color:lightblue
+}
+a {
+    color:green
+}
+#welcome {
+    text-align: center;
+}
+</style>
